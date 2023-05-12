a9029c54fc7c29272f0de51613d73e13453efae7
sonnt4 <sonnt4@runsystem.net>
2023-05-12 16:14:14 +0700

    add test file

---
+++ tests/Unit/PostRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PostRepositoryTest extends TestCase
+{
+    public function test_create()
+    {
+        // 1. Define the goal
+        // 2. Replicate the env / restriction
+
+        // 3. Define the source of truth
+
+        // 4. Compare the result
+    }
+
+    public function test_update()
+    {
+
+    }
+
+    public function test_delete()
+    {
+
+    }
+}
