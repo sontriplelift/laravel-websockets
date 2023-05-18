0ade187efb1e9b6d91359062ce8ddef19e3ed8dc
sonnt4 <sonnt4@runsystem.net>
2023-05-18 17:38:39 +0700

    fortify email verification

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             // 'throttle:api',
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'verified',
         ],
     ];
 
